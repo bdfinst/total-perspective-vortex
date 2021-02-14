406a55e754da5e9143ce343fc4cbd97a611cd83d
bdfinst <bdfinst@gmail.com>
2021-02-14 07:24:48 -0600

    Add base theme

---
+++ src/theme.js
@@ -0,0 +1,83 @@
+import { createMuiTheme } from '@material-ui/core/styles'
+
+// Create a theme instance.
+const theme = createMuiTheme({
+  background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+  palette: {
+    type: 'dark',
+    primary: {
+      main: '#0f5892',
+    },
+    secondary: {
+      main: '#f50057',
+    },
+  },
+  overrides: {
+    MuiButton: {
+      root: {
+        // background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+        border: 0,
+        borderRadius: 3,
+        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+        color: 'white',
+        height: 48,
+        padding: '0 30px',
+      },
+    },
+  },
+  props: {
+    MuiList: {
+      dense: true,
+    },
+    MuiMenuItem: {
+      dense: true,
+    },
+    MuiTable: {
+      size: 'small',
+    },
+    MuiButton: {
+      size: 'small',
+    },
+    MuiButtonGroup: {
+      size: 'small',
+    },
+    MuiCheckbox: {
+      size: 'small',
+    },
+    MuiFab: {
+      size: 'small',
+    },
+    MuiFormControl: {
+      margin: 'dense',
+      size: 'small',
+    },
+    MuiFormHelperText: {
+      margin: 'dense',
+    },
+    MuiIconButton: {
+      size: 'small',
+    },
+    MuiInputBase: {
+      margin: 'dense',
+    },
+    MuiInputLabel: {
+      margin: 'dense',
+    },
+    MuiRadio: {
+      size: 'small',
+    },
+    MuiSwitch: {
+      size: 'small',
+    },
+    MuiTextField: {
+      margin: 'dense',
+      size: 'small',
+    },
+  },
+  shape: {
+    borderRadius: 4,
+  },
+  spacing: 8,
+})
+
+export default theme
