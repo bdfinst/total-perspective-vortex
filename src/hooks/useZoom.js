b303af69851d6079149bb6435eb4e6f9ddec4e52
Bryan Finster <Bryan.Finster@walmart.com>
2021-03-04 15:55:47 -0600

    Add ability to zoom to node with context menu

---
+++ src/hooks/useZoom.js
@@ -0,0 +1,25 @@
+import { useStore, useZoomPanHelper } from 'react-flow-renderer'
+
+const useZoom = () => {
+  const { fitView, setCenter } = useZoomPanHelper()
+  const store = useStore()
+  const { nodes } = store.getState()
+
+  const zoom = (node) => {
+    const zoomNode = nodes.find((el) => el.id === node.id)
+
+    console.log(zoomNode)
+    if (zoomNode) {
+      const x = zoomNode.__rf.position.x + zoomNode.__rf.width / 2
+      const y = zoomNode.__rf.position.y + zoomNode.__rf.height / 2
+      const zoomFactor = 1.85
+
+      setCenter(x, y, zoomFactor)
+    } else {
+      fitView()
+    }
+  }
+  return zoom
+}
+
+export default useZoom
