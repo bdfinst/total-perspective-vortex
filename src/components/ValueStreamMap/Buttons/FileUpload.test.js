98d696a540f88db18fcbb743291e20c7e811b74e
Bryan Finster <Bryan.Finster@walmart.com>
2021-03-05 16:24:53 -0600

    Add file upload test

---
+++ src/components/ValueStreamMap/Buttons/FileUpload.test.js
@@ -0,0 +1,23 @@
+import { fireEvent, getByText, render, screen } from '@testing-library/react'
+import React from 'react'
+
+import { ValueStreamProvider } from '../valueStreamContext'
+import FileUpload from './FileUpload'
+
+it('should open the upload dialog', async () => {
+  const { getByRole, debug } = render(
+    <ValueStreamProvider>
+      <FileUpload />
+    </ValueStreamProvider>,
+  )
+
+  const button = getByRole('button')
+
+  await fireEvent.click(button)
+
+  debug()
+
+  expect(
+    screen.getByText('Drag and drop a file here or click'),
+  ).toBeInTheDocument()
+})
