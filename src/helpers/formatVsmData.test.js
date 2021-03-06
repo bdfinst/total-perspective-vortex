f8d91313e004cb32c7d92b9adae83c51cb27498d
bdfinst <bdfinst@gmail.com>
2021-03-06 08:24:30 -0600

    Convert data to file

--- src/helpers/formatVsmData.test.js
+++ src/helpers/formatVsmData.test.js
@@ -1,7 +1,11 @@
 import _ from 'lodash'
 
 import { buildEdge } from './buildEdge'
-import { buildElementsFromFile } from './formatVsmData'
+import {
+  buildElementsFromFile,
+  buildFileFromElements,
+  isValidFile,
+} from './formatVsmData'
 import { buildNode } from './buildNode'
 import isSameJsonSchema from './isSameJsonSchema'
 
@@ -40,9 +44,48 @@ describe('Converting loaded data to elements', () => {
   const edge = buildEdge(nodes[0], nodes[1])
   const elements = nodes.concat(edge)
 
-  it('should produce a node element', () => {
+  it('should convert the file to elements', () => {
     const newElements = buildElementsFromFile(dataFile)
 
     expect(isSameJsonSchema(elements, newElements)).toEqual(true)
+  })
+  it('should convert the elements to the file formet', () => {
+    const converted = buildFileFromElements(elements)
+
+    expect(isSameJsonSchema(converted, dataFile)).toEqual(true)
+  })
+  describe('Idnetifying bad files', () => {
+    it('should reject a data file that is not an array', () => {
+      expect(isValidFile({})).toEqual(false)
+      expect(isValidFile([])).toEqual(true)
+    })
+    it('should reject a data file that does not contain nodes', () => {
+      expect(isValidFile({})).toEqual(false)
+    })
+    it('should reject a data file that has bad nodes', () => {
+      const badFile = [
+        {
+          id: '1',
+          data: {
+            processName: '',
+            people: 0,
+            waitTime: 0,
+            pctCompleteAccurate: 100,
+          },
+        },
+      ]
+      expect(isValidFile(badFile)).toEqual(false)
+    })
+    it('should reject a data file that has bad edges', () => {
+      const badFile = [
+        {
+          target: '2',
+        },
+      ]
+      expect(isValidFile(badFile)).toEqual(false)
+    })
+    it('should accept a good data file', () => {
+      expect(isValidFile(dataFile)).toEqual(true)
+    })
   })
 })
